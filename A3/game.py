9321a1879c99062c07f1298ac9cf144cb5abbd25
MiguelWolff <149209042+MiguelWolff@users.noreply.github.com>
2025-05-12 20:07:35 -0300

    Add files via upload

---
+++ A3/game.py
@@ -0,0 +1,7 @@
+import pygame as pg
+from obj import Obj
+import random
+
+class Game:
+    def __init__(self):
+        pass
---
+++ A3/obj.py
@@ -0,0 +1,18 @@
+import pygame as pg
+
+class Obj:
+        
+    def __init__(self, image, x, y):
+        
+        self.group = pg.sprite.Group()
+        self.sprite = pg.sprite.Sprite(self.group)
+
+        self.sprite.image = pg.image.load(image)
+        self.sprite.rect = self.sprite.image.get_rect()
+        self.sprite.rect[0] = x
+        self.sprite.rect[1] = y
+        self.frame = 1
+        self.tick = 0
+    
+    def drawing(self, window):
+        self.group.draw(window)
